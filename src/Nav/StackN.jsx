229c9f2d0e57a539a7949e1dc788191514addf0f
MOHTASHIM <hassan.mohtashim12@gmail.com>
2024-12-12 13:08:59 +0500

    Updated Changes

--- src/Nav/StackN.jsx
+++ src/Nav/StackN.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import Onboarding from "../Screen/Onboarding";
-import Home from "../Screen/Home";
+import ProfileScreen from "../Screen/ProfileScreen";
 import Login from "../Screen/Login";
 import SignUp from "../Screen/SignUp";
 import ForgotPassword from "../Screen/ForgotPassword";
@@ -12,7 +12,7 @@ const StackN = () => {
   return (
     <Stack.Navigator screenOptions={{headerShown:false}}>
       <Stack.Screen name="Onboarding" component={Onboarding} />
-      <Stack.Screen name="Home" component={Home}/>
+      <Stack.Screen name="ProfileScreen" component={ProfileScreen}/>
       <Stack.Screen name="Login" component={Login}/>
       <Stack.Screen name="SignUp" component={SignUp}/>
       <Stack.Screen name="ForgotPassword" component={ForgotPassword}/>
